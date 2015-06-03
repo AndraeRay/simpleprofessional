d902cfd18e07b3a95b01af86a452733624b806dd
Andrae Raymond <draeisme@gmail.com>
2015-06-02 21:18:42 -0500

    Added layout files, and set up two column layout

--- footer.php
+++ footer.php
@@ -8,15 +8,16 @@
  */
 ?>
 
-	</div><!-- #content -->
+		</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simpleprofessional' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'simpleprofessional' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'simpleprofessional' ), 'Simple Professional', '<a href="http://raymondinmotion.com" rel="designer">Andrae Raymond</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simpleprofessional' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'simpleprofessional' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'simpleprofessional' ), 'Simple Professional', '<a href="http://raymondinmotion.com" rel="designer">Andrae Raymond</a>' ); ?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div> <!-- page inner -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
