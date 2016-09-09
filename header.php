6e8c926373122e52cd39a479ce63ea953765c961
Andrae Raymond <draeisme@gmail.com>
2016-09-09 16:46:54 -0500

    Added mobile menu icon and styles

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 			</span>
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'simpleprofessional' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( '', 'simpleprofessional' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 			</nav><!-- #site-navigation -->
 		</header><!-- #masthead -->
