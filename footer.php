c59f1b465c5c67831f6baca82f242224203c6cad
Andrae Raymond <draeisme@gmail.com>
2016-09-19 15:11:05 -0500

    change author link to new site

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 			<div class="site-info">
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simpleprofessional' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'simpleprofessional' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( __( 'Theme: %1$s by %2$s.', 'simpleprofessional' ), 'Simple Professional', '<a href="http://raymondinmotion.com" rel="designer">Andrae Raymond</a>' ); ?>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'simpleprofessional' ), 'Simple Professional', '<a href="http://andraeraymond.com" rel="designer">Andrae Raymond</a>' ); ?>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div> <!-- page inner -->
