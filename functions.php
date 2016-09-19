5bc5b4b6c3f770c773b6191fbf1e23d940f6e986
Andrae Raymond <draeisme@gmail.com>
2016-09-19 14:31:16 -0500

    Added 2 extra sidebars, gave user option of picking sidebar

--- functions.php
+++ functions.php
@@ -85,16 +85,30 @@ add_action( 'after_setup_theme', 'simpleprofessional_setup' );
  */
 function simpleprofessional_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'simpleprofessional' ),
+		'name'          => __( 'Default Sidebar', 'simpleprofessional' ),
 		'id'            => 'sidebar-1',
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
+
+	$args = array(
+		'name'          => __('SP Sidebar %d'),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>' 
+	); 
+
+	register_sidebars( 2, $args );
 }
 add_action( 'widgets_init', 'simpleprofessional_widgets_init' );
+
+function simpleprofessional_load_no_sidebar_styles() {
+	wp_enqueue_style( 'content-nosidebar', get_template_directory_uri() . '/layouts/content-nosidebar.css', false, '1.1', 'all' );
+}
 
 /**
  * Enqueue scripts and styles.
--- sidebar.php
+++ sidebar.php
@@ -5,11 +5,26 @@
  * @package Simple Professional
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+$NO_SIDEBAR_ID=99;
+	/* return selected sidebar, and default to 1 */
+	$sidebar = intval(get_field('selected_sidebar'));
+	$sidebar = $sidebar == 0 ? 1 : $sidebar;
+
+		
+	if ( !is_active_sidebar($sidebar) || $sidebar === $NO_SIDEBAR_ID ) {
+		simpleprofessional_load_no_sidebar_styles();
+		return;
+	};
+
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	 
+
+	<?php
+
+	dynamic_sidebar( $sidebar );
+	
+	
+	?>
 </div><!-- #secondary -->
