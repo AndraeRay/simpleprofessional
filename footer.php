484869e844bfe967a4430de24e517c187fde35b4
Andrae Raymond <draeisme@gmail.com>
2017-02-14 16:34:17 -0600

    temporarily removed footer info

--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simpleprofessional' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'simpleprofessional' ), 'WordPress' ); ?></a>
+				<!-- <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simpleprofessional' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'simpleprofessional' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( __( 'Theme: %1$s by %2$s.', 'simpleprofessional' ), 'Simple Professional', '<a href="http://andraeraymond.com" rel="designer">Andrae Raymond</a>' ); ?>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'simpleprofessional' ), 'Simple Professional', '<a href="http://andraeraymond.com" rel="designer">Andrae Raymond</a>' ); ?> -->
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div> <!-- page inner -->
